c890c97bd46859259e3a7c5d868bbad0be5e47c4
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-11-30 11:37:36 -0800

    Pass additional parameters to install script

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -2,4 +2,4 @@
 
 // Script to download and install TAK Server from Artifactory
 wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh
-./install_script_with_postgres.sh $1 $2
+./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
