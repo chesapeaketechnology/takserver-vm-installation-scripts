a2ad1ac56bf54f70b1e7ea258e18a01f34247536
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-11-30 06:57:29 -0800

    Added shebang

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 // Script to download and install TAK Server from Artifactory
 wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh
 ./install_script_with_postgres.sh $1 $2
