e513922c451af8229595b0298b59a78621c1f7d9
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-12-03 14:51:41 -0800

    Debug all variables

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-echo 'Artifactory Username' $1 
-echo 'Organization Name' $3
-echo 'Organization State' $4
-echo 'Organization City' $5
-echo 'Certificate of Authority Name' $6 
-echo 'Admin username' $8
-echo 'Clients' $10
+
+while (( $# > 0 ))
+do
+    echo "$1"
+    shift
+done
+
 # Script to download and install TAK Server from Artifactory
 # wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh --no-check-certificate
 # sudo chmod +x install_script_with_postgres.sh 
