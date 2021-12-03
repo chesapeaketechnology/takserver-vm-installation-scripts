c7d825c2d4bb7c5f31ecd1a888a14bf832e72f86
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-12-03 14:27:58 -0800

    Test passing clients to VM

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
-
+echo 'Artifactory Username' $1 
+echo 'Organization Name' $3
+echo 'Organization State' $4
+echo 'Organization City' $5
+echo 'Certificate of Authority Name' $6 
+echo 'Admin username' $8
+echo 'Clients' $10
 // Script to download and install TAK Server from Artifactory
-wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh --no-check-certificate
-sudo chmod +x install_script_with_postgres.sh 
-sudo ./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8 $9
+# wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh --no-check-certificate
+# sudo chmod +x install_script_with_postgres.sh 
+# sudo ./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 $10
