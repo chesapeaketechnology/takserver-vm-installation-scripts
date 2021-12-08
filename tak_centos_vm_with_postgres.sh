1735fd1f7eb19217df00c369684c72dbb4647cf3
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-12-08 10:22:38 -0800

    Restore script functionality

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -3,4 +3,4 @@
 # Script to download and install TAK Server from Artifactory
 wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh --no-check-certificate
 sudo chmod +x install_script_with_postgres.sh 
-# sudo ./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8 $9 ${10}
+sudo ./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8
