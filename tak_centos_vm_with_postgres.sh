af58ea50fbcf7e8ab3af1e7903472d96302a9c4c
CorriganRJ <31051342+CorriganRJ@users.noreply.github.com>
2021-12-15 09:09:59 -0800

    Update tak_centos_vm_with_postgres.sh

--- tak_centos_vm_with_postgres.sh
+++ tak_centos_vm_with_postgres.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 # Script to download and install TAK Server from Artifactory
-echo $@
 wget --user=$1 --password=$2 https://artifacts.tak.gov:443/artifactory/generic-devtest-local/tak/azure_vm/install_script_with_postgres.sh --no-check-certificate
 sudo chmod +x install_script_with_postgres.sh 
 #sudo ./install_script_with_postgres.sh $1 $2 $3 $4 $5 $6 $7 $8
